5bda878179f8103e652c4ae3f0daacf21f5ce49b
Mihir <cse210001050@iiti.ac.in>
2023-06-20 13:00:07 +0530

    added: drag and drop with some constraints

--- chessgame/src/board/boardSquare.jsx
+++ chessgame/src/board/boardSquare.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import './Tiles.css';
 
 function BoardSquare(props) {
 
-  
+  //pieces imagea are made as background of div\
+  let check=true;
+  if(props.image===undefined)
+  {
+    check=false;
+  } 
   return (
-    <div className='w-[70px] h-[70px] text-blue-600' style={{backgroundColor: props.bgcolor}}>
+    <div className='w-[70px] h-[70px] text-blue-600' id='tiles' style={{backgroundColor: props.bgcolor}}>
       
-      <div className='square' style={{backgroundImage: `url(${props.image})`, backgroundRepeat: 'no-repeat', width: "70px", height: "70px"}}>
-      </div>
+      {check && 
+        <div className='square' style={{backgroundImage: `url(${props.image})`, backgroundRepeat: 'no-repeat', width: "70px", height: "70px"}}>
+        </div>
+      }
+      
     </div>
   )
 }
--- chessgame/src/board/chessBoard.jsx
+++ chessgame/src/board/chessBoard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import BoardSquare from "./boardSquare";
 import knight_w from "../assets/knight_w.png";
 import knight_b from "../assets/knight_b.png";
@@ -14,7 +14,10 @@ import rook_b from "../assets/rook_b.png";
 import rook_w from "../assets/rook_w.png";
 
 function ChessBoard() {
+
+  const chessboardref=useRef(null);
   const tiles = [];
+  //these are the initial postions of chess board
   const initial_pos = {
     "0a": rook_b,
     "0b": knight_b,
@@ -50,9 +53,14 @@ function ChessBoard() {
     "6h": pawn_w,
   };
 
+  //vertical axis markings
   const change = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  //horizantal axis markings 0, 1, 2, 3.....so on
+
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
+
+
       if ((i + j) % 2 == 0) {
         let s = j + change[i];
         tiles.push(
@@ -76,12 +84,57 @@ function ChessBoard() {
           />
         );
       }
+
+      
+    }
+  }
+  
+  let activeElement=undefined;
+
+  function grabPiece(e){ 
+    const element=e.target;
+    if(element.className=='square')
+    {
+      activeElement=e;
+      element.style.position="absolute";
+      let x=e.clientX-35;
+      let y=e.clientY-35;
+      element.style.left=`${x}px`;
+      element.style.top=`${y}px`;
+      // console.log(element);
     }
   }
 
+  function movePiece(e){
+    // console.log(e.target);
+    if(activeElement)
+    {
+
+      const element=activeElement.target;
+      //constraints to keep the chess pieces on board only  
+        const minX=chessboardref.current.offsetLeft;
+        const minY=chessboardref.current.offsetTop;
+        const maxX=chessboardref.current.offsetLeft+490;
+        const maxY=chessboardref.current.offsetTop+490;
+      
+        element.style.position="absolute";
+        let x=e.clientX-35;
+        let y=e.clientY-35;
+        element.style.left=x>minX&&x<maxX ? `${x}px` : x<minX ? `${minX}px` : `${maxX}px`;
+        element.style.top=y>minY&&y<maxY ? `${y}px` : y<minY ? `${minY}px` : `${maxY}px`;
+    }
+    
+  }
+
+  function ungrab(e){
+    if(activeElement)
+    {
+      activeElement=undefined;
+    }
+  }
   return (
     <div className="bg-stone-800 h-screen grid content-center align-middle drop-shadow-2xl">
-      <div className={`grid grid-rows-8 grid-flow-col w-[560px] m-auto`}>
+      <div onMouseUp={(e)=>{ungrab(e)}} onMouseMove={(e)=>{movePiece(e)}} onMouseDown={(e)=>grabPiece(e)} className={`grid grid-rows-8 grid-flow-col w-[560px] m-auto`} ref={chessboardref}>
         {tiles}
       </div>
     </div>
