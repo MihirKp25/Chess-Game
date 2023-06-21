94d624d9cb945f20dc1cb63897325791bf5f5b66
Mihir <cse210001050@iiti.ac.in>
2023-06-21 23:18:31 +0530

    fen string can be now converted to chess pieces

---
+++ chessgame/src/Fen/Fenconversion.js
@@ -0,0 +1,189 @@
+import knight_w from "../assets/knight_w.png";
+import knight_b from "../assets/knight_b.png";
+import bishop_w from "../assets/bishop_w.png";
+import bishop_b from "../assets/bishop_b.png";
+import king_b from "../assets/king_b.png";
+import king_w from "../assets/king_w.png";
+import pawn_w from "../assets/pawn_w.png";
+import pawn_b from "../assets/pawn_b.png";
+import queen_w from "../assets/queen_w.png";
+import queen_b from "../assets/queen_b.png";
+import rook_b from "../assets/rook_b.png";
+import rook_w from "../assets/rook_w.png";
+
+const pieces = {
+  p: pawn_b,
+  P: pawn_w,
+  b: bishop_b,
+  B: bishop_w,
+  k: king_b,
+  K: king_w,
+  n: knight_b,
+  N: knight_w,
+  q: queen_b,
+  Q: queen_w,
+  r: rook_b,
+  R: rook_w,
+};
+
+const board = {
+  "0a": null,
+  "0b": null,
+  "0c": null,
+  "0d": null,
+  "0e": null,
+  "0f": null,
+  "0g": null,
+  "0h": null,
+  "1a": null,
+  "1b": null,
+  "1c": null,
+  "1d": null,
+  "1e": null,
+  "1f": null,
+  "1g": null,
+  "1h": null,
+  "2a": null,
+  "2b": null,
+  "2c": null,
+  "2d": null,
+  "2e": null,
+  "2f": null,
+  "2g": null,
+  "2h": null,
+  "3a": null,
+  "3b": null,
+  "3c": null,
+  "3d": null,
+  "3e": null,
+  "3f": null,
+  "3g": null,
+  "3h": null,
+  "4a": null,
+  "4b": null,
+  "4c": null,
+  "4d": null,
+  "4e": null,
+  "4f": null,
+  "4g": null,
+  "4h": null,
+  "5a": null,
+  "5b": null,
+  "5c": null,
+  "5d": null,
+  "5e": null,
+  "5f": null,
+  "5g": null,
+  "5h": null,
+  "6a": null,
+  "6b": null,
+  "6c": null,
+  "6d": null,
+  "6e": null,
+  "6f": null,
+  "6g": null,
+  "6h": null,
+  "7a": null,
+  "7b": null,
+  "7c": null,
+  "7d": null,
+  "7e": null,
+  "7f": null,
+  "7g": null,
+  "7h": null,
+};
+
+let arr = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+];
+const change = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+export function fen_to_state(s) {
+  let cnt = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "/") {
+      if (s[i].toUpperCase() === s[i].toLowerCase()) {
+        let x = parseInt(s[i]);
+        cnt += x;
+      } else {
+        arr[cnt] = pieces[s[i]];
+        cnt++;
+      }
+    }
+  }
+  let t=0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+        let s = i + change[j];
+        board[s]=arr[t];
+        t++;
+    }
+  }
+
+  return board;
+  console.log(board);
+  
+}
--- chessgame/src/board/chessBoard.jsx
+++ chessgame/src/board/chessBoard.jsx
@@ -12,50 +12,88 @@ import queen_w from "../assets/queen_w.png";
 import queen_b from "../assets/queen_b.png";
 import rook_b from "../assets/rook_b.png";
 import rook_w from "../assets/rook_w.png";
+import { fen_to_state } from "../Fen/Fenconversion";
 
 function ChessBoard() {
 
   const chessboardref=useRef(null);
   const tiles = [];
-  //these are the initial postions of chess board
-  const initial_pos = {
-    "0a": rook_b,
-    "0b": knight_b,
-    "0c": bishop_b,
-    "0d": queen_b,
-    "0e": king_b,
-    "0f": bishop_b,
-    "0g": knight_b,
-    "0h": rook_b,
-    "1a": pawn_b,
-    "1b": pawn_b,
-    "1c": pawn_b,
-    "1d": pawn_b,
-    "1e": pawn_b,
-    "1f": pawn_b,
-    "1g": pawn_b,
-    "1h": pawn_b,
-    "7a": rook_w,
-    "7b": knight_w,
-    "7c": bishop_w,
-    "7d": queen_w,
-    "7e": king_w,
-    "7f": bishop_w,
-    "7g": knight_w,
-    "7h": rook_w,
-    "6a": pawn_w,
-    "6b": pawn_w,
-    "6c": pawn_w,
-    "6d": pawn_w,
-    "6e": pawn_w,
-    "6f": pawn_w,
-    "6g": pawn_w,
-    "6h": pawn_w,
+  //this will be the temporary representation of chess board
+  let chessboard = {
+    "0a": null,
+    "0b": null,
+    "0c": null,
+    "0d": null,
+    "0e": null,
+    "0f": null,
+    "0g": null,
+    "0h": null,
+    "1a": null,
+    "1b": null,
+    "1c": null,
+    "1d": null,
+    "1e": null,
+    "1f": null,
+    "1g": null,
+    "1h": null,
+    "2a": null,
+    "2b": null,
+    "2c": null,
+    "2d": null,
+    "2e": null,
+    "2f": null,
+    "2g": null,
+    "2h": null,
+    "3a": null,
+    "3b": null,
+    "3c": null,
+    "3d": null,
+    "3e": null,
+    "3f": null,
+    "3g": null,
+    "3h": null,
+    "4a": null,
+    "4b": null,
+    "4c": null,
+    "4d": null,
+    "4e": null,
+    "4f": null,
+    "4g": null,
+    "4h": null,
+    "5a": null,
+    "5b": null,
+    "5c": null,
+    "5d": null,
+    "5e": null,
+    "5f": null,
+    "5g": null,
+    "5h": null,
+    "6a": null,
+    "6b": null,
+    "6c": null,
+    "6d": null,
+    "6e": null,
+    "6f": null,
+    "6g": null,
+    "6h": null,
+    "7a": null,
+    "7b": null,
+    "7c": null,
+    "7d": null,
+    "7e": null,
+    "7f": null,
+    "7g": null,
+    "7h": null,
   };
 
   //vertical axis markings
   const change = ["a", "b", "c", "d", "e", "f", "g", "h"];
   //horizantal axis markings 0, 1, 2, 3.....so on
+
+  //enter your fen dtring here to make initial setup
+  // "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" for initial position
+  chessboard=fen_to_state("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+
 
   for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
@@ -69,7 +107,7 @@ function ChessBoard() {
             bgcolor={"#769656"}
             x={j}
             y={change[i]}
-            image={initial_pos[s]}
+            image={chessboard[s]}
           />
         );
       } else {
@@ -80,7 +118,7 @@ function ChessBoard() {
             bgcolor={"#eeeed2"}
             x={j}
             y={change[i]}
-            image={initial_pos[s]}
+            image={chessboard[s]}
           />
         );
       }
